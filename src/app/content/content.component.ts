daa1275cb7d44fa084afda89330d03eef99d1d98
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2021-08-25 18:58:07 +0900

    main

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContentComponent implements OnInit {
   loginform: FormGroup;

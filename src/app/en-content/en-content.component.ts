7408f182ee3018851ac77c52e28c1fd96b8b9c94
Kent Nagao <mortpanda@gmail.com>
2021-08-27 10:08:58 +0000

    main

--- src/app/en-content/en-content.component.ts
+++ src/app/en-content/en-content.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "app/shared/okta/okta-authentication";
+import { OktaAuth } from "@okta/okta-auth-js";
+import { ViewEncapsulation } from '@angular/core';
+import { OktaSDKAuthService } from 'app/shared/okta/okta-auth-service';
+import { DOCUMENT } from '@angular/common';
+import CryptoJS from 'crypto-js';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-en-content',
   templateUrl: './en-content.component.html',
-  styleUrls: ['./en-content.component.css']
+  styleUrls: ['./en-content.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EnContentComponent implements OnInit {
 
-  constructor() { }
+  uriHibp = 'https://api.pwnedpasswords.com/range/';
+  results: string[];
+  loginform: FormGroup;
+  public loginInvalid: boolean;
+  private formSubmitAttempt: boolean;
+  private returnUrl: string;
+  
+  
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private oktaSDKAuth: OktaSDKAuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    
   }
 
 }
